14acfb725467e07faebb89b23f852e5645cfe3d2
Alysha Columbres <alysha.columbres@obf.ateneo.edu>
2020-03-06 23:23:45 +0800

    created CBVs for each url in heroes and created an html file for heroes

--- heroes/tests.py
+++ heroes/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TaskListPageTest(TestCase):
+
+	def test_uses_list_template(self):
+		response = self.client.get('/heroes')
+		self.assertTemplateUsed(response, 'heroes.html')
+
+		response = self.client.get('/hero/cloud')
+		self.assertTemplateUsed(response, 'detail_cloud.html')
+
+		response = self.client.get('/hero/sunflowey')
+		self.assertTemplateUsed(response, 'detail_sunflowey.html')
+
+		response = self.client.get('/hero/jester')
+		self.assertTemplateUsed(response, 'detail_jester.html')
--- heroes/views.py
+++ heroes/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+class Heroes(TemplateView):
+	template_name = "heroes.html"
+
+
+class Cloud(TemplateView):
+	template_name = "detail_cloud.html"
+
+
+class Sunflowey(TemplateView):
+	template_name = "detail_sunflowey.html"
+
+
+class Jester(TemplateView):
+	template_name = "detail_jester.html"
