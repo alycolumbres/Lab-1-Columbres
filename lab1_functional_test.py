aa5015c3e8af396271f3a72e2e0c7a4cf5525e3e
Alysha Columbres <alysha.columbres@obf.ateneo.edu>
2020-03-06 23:44:38 +0800

    edited the functional test file with code from tests.py

--- lab1_functional_test.py
+++ lab1_functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from django.test import TestCase
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,15 +18,30 @@ class NewVisitorTest(unittest.TestCase):
 		# 'The Will of the Wisps Wiki'
 		self.assertIn('The Will of the Wisps Wiki', self.browser.title)
 		
+	def test_uses_list_template(self):
 		# She sees a list containing three heroes with their corresponding 
 		# names, health points, and damage 
+		response = self.client.get('/heroes')
+		self.assertTemplateUsed(response, 'heroes.html')
 		
 		# When she selects one of the heroes, she is sent to another page
 		# containing more information about the hero (additional stats, lore, image).
-		
+		response = self.client.get('/hero/cloud')
+		self.assertTemplateUsed(response, 'detail_cloud.html')
 		# She spots the page title and header mentions the name of the hero she selected.
+		self.assertIn('Cloud', self.browser.title)
+
+		response = self.client.get('/hero/sunflowey')
+		self.assertTemplateUsed(response, 'detail_sunflowey.html')
+		self.assertIn('Sunflowey', self.browser.title)
+
+		response = self.client.get('/hero/jester')
+		self.assertTemplateUsed(response, 'detail_jester.html')
+		self.assertIn('Jester', self.browser.title)
 		
 		# While she is in a specific hero's page, she sees a button labeled "Back to Heroes List".
-		# She clicks this and she is redirected back to the wiki's homepage.	
+		# She clicks this and she is redirected back to the wiki's homepage.
+		self.assertIn('Back to Heroes List', html)
+		self.assertTemplateUsed(response, 'heroes.html')
 		
 		self.fail('Finish the test!')
