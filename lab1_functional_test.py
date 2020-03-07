5b519bdbfaaf941385ae3b4fa592898849aa5d9b
Alysha Columbres <alysha.columbres@obf.ateneo.edu>
2020-03-07 18:39:45 +0800

    added id tags for all heroes and edited names

--- lab1_functional_test.py
+++ lab1_functional_test.py
@@ -12,7 +12,7 @@ class NewVisitorTest(unittest.TestCase):
 	def test_can_display_a_heroes_list_and_more_information_per_hero(self):
 		# Widget has heard about a new wiki app for the game called The Will of the Wisps. 
 		# She goes to check out its homepage
-		self.browser.get('http://localhost:8000')
+		self.browser.get('http://localhost:8000/heroes')
 
 		# She notices the page title and header mention 
 		# 'The Will of the Wisps Wiki'
@@ -20,22 +20,22 @@ class NewVisitorTest(unittest.TestCase):
 		
 		# She sees a list containing three heroes with their corresponding 
 		# names, health points, and damage 
-		seld.assertIn(self.browser.current_url,
+		self.assertIn(self.browser.current_url,
 			'http://localhost:8000/heroes')
 		
 		# When she selects one of the heroes, she is sent to another page
 		# containing more information about the hero (additional stats, lore, image).
 		self.browser.get('http://localhost:8000/hero/cloud')
 
-		self.assertIn('Health Points', self.browser.find_element_by_id('hero_health').text)
+		self.assertIn('Health Points', self.browser.find_element_by_id('health').text)
 
-		self.assertIn('Base Attack Damage', self.browser.find_element_by_id('hero_attack').text)
+		self.assertIn('Base Attack Damage', self.browser.find_element_by_id('attack').text)
 
-		self.assertIn('Skills', self.browser.find_element_by_id('hero_skills').text)
+		self.assertIn('Skills', self.browser.find_element_by_id('skills').text)
 
-		self.assertIn('Lore', self.browser.find_element_by_id('hero_lore').text)
+		self.assertIn('Lore', self.browser.find_element_by_id('lore').text)
 
-		self.assertIn('.png', self.browser.find_element_by_id('hero_image').get_attribute('src'))
+		self.assertIn('.png', self.browser.find_element_by_id('image').get_attribute('src'))
 		# She spots the page title and header mentions the name of the hero she selected.
 		self.assertIn('Detail - ', self.browser.title)
 		
