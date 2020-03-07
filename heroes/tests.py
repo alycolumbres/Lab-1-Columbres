08848fc5cc4b2c2fc99a84c2955eaa761c6c7c54
Alysha Columbres <alysha.columbres@obf.ateneo.edu>
2020-03-07 18:32:20 +0800

    edited tests.py in heroes folder

--- heroes/tests.py
+++ heroes/tests.py
@@ -1,17 +1,26 @@
 from django.test import TestCase
 
 # Create your tests here.
-class TaskListPageTest(TestCase):
+class TestForHeroes(TestCase):
 
-	def test_uses_list_template(self):
+	def test_heroes_if_correct_response_(self):
 		response = self.client.get('/heroes')
 		self.assertTemplateUsed(response, 'heroes.html')
 
+class TestForHeroCloud(TestCase):
+
+	def test_hero_cloud_if_correct_response_(self):
 		response = self.client.get('/hero/cloud')
 		self.assertTemplateUsed(response, 'detail_cloud.html')
 
+class TestForHeroSunflowey(TestCase):
+
+	def test_hero_sunflowey_if_correct_response_(self):
 		response = self.client.get('/hero/sunflowey')
 		self.assertTemplateUsed(response, 'detail_sunflowey.html')
 
+class TestForHeroJester(TestCase):
+
+	def test_hero_jester_if_correct_response_(self):
 		response = self.client.get('/hero/jester')
 		self.assertTemplateUsed(response, 'detail_jester.html')
